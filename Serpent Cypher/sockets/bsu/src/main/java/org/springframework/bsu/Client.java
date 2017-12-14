2cd7b79e1198e3d4f2abf416868c6dc037658f9d
EXADEL\avertsinskaya <avertsinskaya@exadel.com>
2017-12-14 22:46:09 +0300

    Serpent: add illustration of authentication, clean up

--- Serpent Cypher/sockets/bsu/src/main/java/org/springframework/bsu/Client.java
+++ Serpent Cypher/sockets/bsu/src/main/java/org/springframework/bsu/Client.java
@@ -6,9 +6,11 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.Socket;
@@ -21,6 +23,7 @@ import java.util.Arrays;
 import java.util.Base64;
 
 import javax.crypto.Cipher;
+import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -31,91 +34,126 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class Client {
-	
-	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, IOException {
+
+	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, IOException, NoSuchAlgorithmException {
 		Thread frameThread = new Thread(new FrameAssistant("Chat"));
 		frameThread.start();
 	}
 }
 
-class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox{
+class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox {
 	private static final long serialVersionUID = 1L;
 	JTextArea textAreaInput;
-	JButton requestTextBtn;
-	JPanel cardPanel, buttonPanel;
+	JTextArea loginArea, passwordArea;
+	JButton requestTextBtn, loginBtn, okBtn;
+	JPanel cardPanel, buttonPanel, loginPanel;
 	Font font = new Font("Verdana", Font.PLAIN, 20);
 	
+	PasswordSHAEncryption passwordEncrypter = new PasswordSHAEncryption();
+
 	KeyPairGenerator keyGen;
 	KeyPair key;
-	
+
 	Socket clientSocket = new Socket("192.168.100.6", 8092);
 	ObjectOutputStream outputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 	ObjectInputStream inputStream = new ObjectInputStream(clientSocket.getInputStream());
 	CommonCryption common = new CommonCryption();
-	
+
 	public FrameAssistant(String s) throws FileNotFoundException, ClassNotFoundException, IOException {
 		super(s);
-		
 		cardPanel = new JPanel(new CardLayout());
+
+		loginPanel = createLoginPanel();
 		
+		cardPanel.add(loginPanel);
 		cardPanel.add(createTextInputPanel(), BorderLayout.CENTER);
-		
+
 		this.getContentPane().add(cardPanel);
-		
+
 		this.setSize(700, 500);
 		this.setVisible(true);
-		
+
 		closeWindow();
 	}
-	
+
 	public void closeWindow() {
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		this.addWindowListener(new java.awt.event.WindowAdapter() {
-		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-		        try {
-		        	inputStream.close();
-		        	outputStream.close();
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				try {
+					inputStream.close();
+					outputStream.close();
 					clientSocket.close();
 					System.exit(0);
 				} catch (IOException e) {
 					System.out.println(e);
 				}
-		    }
+			}
 		});
 	}
-	
+
 	private void generateRSAKeys() throws NoSuchAlgorithmException {
-		 keyGen = KeyPairGenerator.getInstance("RSA");
-	     keyGen.initialize(512);
-	     
-	     key = keyGen.generateKeyPair();
+		keyGen = KeyPairGenerator.getInstance("RSA");
+		keyGen.initialize(512);
+
+		key = keyGen.generateKeyPair();
 	}
-	
+
 	private JPanel createTextInputPanel() {
 		JPanel panel = new JPanel();
-		
+
 		textAreaInput = new JTextArea(100, 50);
-		
-		JScrollPane areaScrollPane = new JScrollPane(textAreaInput, 
-				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+
+		JScrollPane areaScrollPane = new JScrollPane(textAreaInput, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
 				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		areaScrollPane.setPreferredSize(new Dimension(433, 400));
-		
+
 		textAreaInput.setLineWrap(true);
 		textAreaInput.setFont(font);
-		
+
 		requestTextBtn = new JButton("Request Text");
 		requestTextBtn.addActionListener(this);
-		
+
 		panel.add(areaScrollPane);
 		panel.add(requestTextBtn);
-		
+
 		return panel;
 	}
-	
-	
+
+	private JPanel createLoginPanel() {
+		JPanel panel = new JPanel(new GridLayout(3, 2));
+
+		JLabel loginLabel = new JLabel("Login");
+		JLabel passwordLabel = new JLabel("Password");
+
+		loginArea = new JTextArea(2, 15);
+		loginArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+		passwordArea = new JTextArea(2, 15);
+		passwordArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+		okBtn = new JButton("Ok");
+		okBtn.addActionListener(this);
+
+		panel.add(createFlowLayoutPanel(loginLabel));
+		panel.add(createFlowLayoutPanel(loginArea));
+		panel.add(createFlowLayoutPanel(passwordLabel));
+		panel.add(createFlowLayoutPanel(passwordArea));
+		panel.add(createFlowLayoutPanel(okBtn));
+
+		return panel;
+	}
+
+	private JPanel createFlowLayoutPanel(Component component) {
+		JPanel panel = new JPanel();
+		component.setFont(font);
+		panel.add(component);
+		return panel;
+	}
+
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == requestTextBtn) {
+		CardLayout cl = (CardLayout) (cardPanel.getLayout());
+		if (e.getSource() == requestTextBtn) {
 			try {
 				textAreaInput.setText("");
 				this.generateRSAKeys();
@@ -130,69 +168,86 @@ class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox{
 			} catch (NoSuchAlgorithmException e1) {
 				System.out.println(e1.getMessage());
 			}
-		} 
+		} else if (e.getSource() == okBtn) {
+			String login = loginArea.getText();
+			String password = passwordArea.getText();
+			
+			// To illustrate authentication with salt and sha1 algorithm
+			// In reality need to store salt and hash in database
+			String requiredHash = "f98a0ea8658d041d28f12b43db3ba1c5d4e061e0";
+			byte[] storedSalt = {-82, 68, 8, 115, 50, -126, 70 , -107, 97, 3, 105, -106, -50, 5, -45, -117, -4};
+			
+			System.out.println(passwordEncrypter.getSHA1Password(password, storedSalt));
+			if (login.equals("nick") && passwordEncrypter.getSHA1Password(password, storedSalt).equals(requiredHash)) {
+				loginArea.setText("");
+				passwordArea.setText("");
+				cl.last(cardPanel);
+			} else {
+				System.out.println("Wrong password or login");
+			}
+		}
 	}
 
 	public void run() {
 		String sessionKey = "";
-		while(true) {
+		while (true) {
 			try {
 				if (inputStream.available() > 0) {
 					byte messageType = inputStream.readByte();
-	
-	        	    switch(messageType) {
-	            	    case 0: // Receive ecnrypted session key
-	            	    	byte[] encryptedSessionKey = Base64.getDecoder().decode(inputStream.readUTF());
-	            	    	sessionKey = this.decryptSessionKey(encryptedSessionKey, key.getPrivate());
-	            	    	System.out.println("Descrypted session key: " + sessionKey);
-	            	    	
-	            	    	// Send text-request flag
-	            	    	outputStream.writeByte(1);
-	        				outputStream.flush();
-	            	    	break;
-	            	    case 1: // Receive encrypted text and text length
-	            	    	Message message = (Message)inputStream.readObject();
-	            	    	String text = message.getText();
-	            	    	String length = message.getTextLength();
-	            	    	int[] arr = Arrays.stream(text.substring(1, text.length()-1).split(","))
-	            	    		    .map(String::trim).mapToInt(Integer::parseInt).toArray();
-	            	    	arr = this.decrypt(this.reverseTextBlocks(arr), sessionKey);
-	            	    	byte[] bytes = this.convertToByteArray(this.reverseTextBlocks(arr));
-	            	    	String decryptedText = new String(bytes);
-	            	    	
-	            	    	arr = Arrays.stream(length.substring(1, length.length()-1).split(","))
-	            	    		    .map(String::trim).mapToInt(Integer::parseInt).toArray();
-	            	    	arr = this.decrypt(this.reverseTextBlocks(arr), sessionKey);
-	            	    	bytes = this.convertToByteArray(this.reverseTextBlocks(arr));
-	            	    	int decryptedTextLength = this.parseEncryptedTextLength(new String(bytes));
-	            	    	
-	            	    	textAreaInput.setText(decryptedText.substring(0, decryptedTextLength));
-	            	    	break;
-	        	    }
+
+					switch (messageType) {
+					case 0: // Receive ecnrypted session key
+						byte[] encryptedSessionKey = Base64.getDecoder().decode(inputStream.readUTF());
+						sessionKey = this.decryptSessionKey(encryptedSessionKey, key.getPrivate());
+						System.out.println("Descrypted session key: " + sessionKey);
+
+						// Send text-request flag
+						outputStream.writeByte(1);
+						outputStream.flush();
+						break;
+					case 1: // Receive encrypted text and text length
+						Message message = (Message) inputStream.readObject();
+						String text = message.getText();
+						String length = message.getTextLength();
+						int[] arr = Arrays.stream(text.substring(1, text.length() - 1).split(",")).map(String::trim)
+								.mapToInt(Integer::parseInt).toArray();
+						arr = this.decrypt(this.reverseTextBlocks(arr), sessionKey);
+						byte[] bytes = this.convertToByteArray(this.reverseTextBlocks(arr));
+						String decryptedText = new String(bytes);
+
+						arr = Arrays.stream(length.substring(1, length.length() - 1).split(",")).map(String::trim)
+								.mapToInt(Integer::parseInt).toArray();
+						arr = this.decrypt(this.reverseTextBlocks(arr), sessionKey);
+						bytes = this.convertToByteArray(this.reverseTextBlocks(arr));
+						int decryptedTextLength = this.parseEncryptedTextLength(new String(bytes));
+
+						textAreaInput.setText(decryptedText.substring(0, decryptedTextLength));
+						break;
+					}
 				}
-			} catch(SocketException e) {
+			} catch (SocketException e) {
 				System.out.println(e.getMessage());
 				textAreaInput.setText("Server was closed");
 				break;
-			} catch(IOException e) {
+			} catch (IOException e) {
 				System.out.println("Stream was closed");
 			} catch (ClassNotFoundException e) {
 				System.out.println(e.getMessage());
 			}
 		}
 	}
-	
+
 	private byte[] convertToByteArray(int[] integers) {
 		byte[] bytes = new byte[integers.length];
-		for(int i = 0; i < integers.length; i ++) {
+		for (int i = 0; i < integers.length; i++) {
 			bytes[i] = (byte) integers[i];
 		}
 		return bytes;
 	}
-	
+
 	private int parseEncryptedTextLength(String length) {
 		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < length.length(); i ++) {
+		for (int i = 0; i < length.length(); i++) {
 			if (String.valueOf(length.charAt(i)).matches("[0-9]")) {
 				builder.append(length.charAt(i));
 			} else {
@@ -201,7 +256,7 @@ class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox{
 		}
 		return Integer.parseInt(builder.toString());
 	}
-	
+
 	private String decryptSessionKey(byte[] ecnryptedSessionKey, PrivateKey key) {
 		byte[] dectyptedText = null;
 		try {
@@ -216,7 +271,7 @@ class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox{
 
 		return new String(dectyptedText);
 	}
-	
+
 	private int[] reverseTextBlocks(int[] blocks) {
 		if (blocks.length == 4) {
 			return blocks;
@@ -225,60 +280,60 @@ class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox{
 		int j = blocks.length - 1;
 		for (int i = 0; i < blocks.length; i += 4) {
 			int[] blocksToCopy = new int[4];
-			
-			for (int k = i; k - i < 4; k ++) {
+
+			for (int k = i; k - i < 4; k++) {
 				blocksToCopy[k - i] = blocks[k];
 			}
-			
-			for (int k = 3; k >= 0; k --) {
+
+			for (int k = 3; k >= 0; k--) {
 				reversedArray[j - k] = blocksToCopy[3 - k];
 			}
-			
+
 			j -= 4;
 		}
 		return reversedArray;
 	}
-	
+
 	private int[] decrypt(int[] texts, String key) {
-    	byte[] binaryKeys = key.getBytes();
+		byte[] binaryKeys = key.getBytes();
 		int[] keys = new int[4];
-		
-		for (int i = 0; i < 4; i ++) {
-			keys[i] = (int)binaryKeys[i];
+
+		for (int i = 0; i < 4; i++) {
+			keys[i] = (int) binaryKeys[i];
 		}
-		
+
 		int randomInitVector = 109;
 		int[] decryptedText = new int[texts.length];
 		for (int i = 0; i < texts.length; i += 4) {
 			int[] partsToDecrypt = new int[4];
-			for (int j = i; j - i < 4; j ++) {
+			for (int j = i; j - i < 4; j++) {
 				partsToDecrypt[j - i] = texts[j];
 			}
-			
+
 			partsToDecrypt = common.getIntegers(common.initPermutation(common.getBytes(partsToDecrypt)));
-			for(int k = 31; k >= 0; k --) {
+			for (int k = 31; k >= 0; k--) {
 				this.linearTransform(partsToDecrypt, keys);
 				partsToDecrypt = common.getIntegers(sBoxInv(common.getBytes(partsToDecrypt), k));
 			}
 			partsToDecrypt = common.getIntegers(common.finalPermutation(common.getBytes(partsToDecrypt)));
-			
+
 			// Cipher feed back mode
-			for (int j = i; j - i < 4; j ++) {
+			for (int j = i; j - i < 4; j++) {
 				if ((i + 4) >= (texts.length - 1)) { // last iteration
 					partsToDecrypt[j - i] = partsToDecrypt[j - i] ^ randomInitVector;
 				} else {
 					partsToDecrypt[j - i] = partsToDecrypt[j - i] ^ texts[j + 4];
 				}
 			}
-			
-			for (int j = i; j - i < 4; j ++) {
+
+			for (int j = i; j - i < 4; j++) {
 				decryptedText[j] = partsToDecrypt[j - i];
 			}
 		}
-		
+
 		return decryptedText;
-    }
-	
+	}
+
 	private int[] linearTransform(int[] b, int[] keys) {
 		b[2] = Integer.rotateRight(b[2], 22);
 		b[0] = Integer.rotateRight(b[0], 5);
@@ -290,24 +345,23 @@ class FrameAssistant extends JFrame implements ActionListener, Runnable, SBox{
 		b[1] = b[0] ^ b[1] ^ b[2];
 		b[2] = Integer.rotateRight(b[2], 3);
 		b[0] = Integer.rotateRight(b[0], 13);
-		for (int i = 0; i < 4; i ++) {
+		for (int i = 0; i < 4; i++) {
 			b[i] = b[i] ^ keys[i];
 		}
 		return b;
 	}
-	
-	private byte[] sBoxInv(byte[] data, int round) {
-        byte[] toUse = inverseBoxes[round%8];
-        byte[] output = new byte[16];
-        for( int i = 0; i < 16; i++ ) {
-            //Break signed-ness
-            int curr = data[i]&0xFF;
-            byte low4 = (byte)(curr>>>4);
-            byte high4 = (byte)(curr&0x0F);
-            output[i] = (byte) ((toUse[low4]<<4) ^ (toUse[high4]));
-        }
-        return output;
-    }
-	
-}
 
+	private byte[] sBoxInv(byte[] data, int round) {
+		byte[] toUse = inverseBoxes[round % 8];
+		byte[] output = new byte[16];
+		for (int i = 0; i < 16; i++) {
+			// Break signed-ness
+			int curr = data[i] & 0xFF;
+			byte low4 = (byte) (curr >>> 4);
+			byte high4 = (byte) (curr & 0x0F);
+			output[i] = (byte) ((toUse[low4] << 4) ^ (toUse[high4]));
+		}
+		return output;
+	}
+
+}
---
+++ Serpent Cypher/sockets/bsu/src/main/java/org/springframework/bsu/PasswordSHAEncryption.java
@@ -0,0 +1,32 @@
+package org.springframework.bsu;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class PasswordSHAEncryption {
+	
+	public byte[] getSalt() throws NoSuchAlgorithmException {
+		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+		byte[] salt = new byte[16];
+		sr.nextBytes(salt);
+		return salt;
+	}
+	
+	public String getSHA1Password(String passwordToHash, byte[] salt) {
+		String generatedPassword = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+			md.update(salt);
+			byte[] bytes = md.digest(passwordToHash.getBytes());
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < bytes.length; i++) {
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			generatedPassword = sb.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+}
